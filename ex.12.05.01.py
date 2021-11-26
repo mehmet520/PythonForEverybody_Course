eb6566313e3f933ca46747d1cfaa75e296857ce2
Mehmet Yilmaz <myilmaz8@web.de>
2021-11-26 19:43:28 +0100

    Python_Course Orijinal-Codes from Dr Chuck

--- ex.12.05.01.py
+++ ex.12.05.01.py
@@ -5,19 +5,19 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
 url= input ('Enter - URL\n')
-print ('\nurl: ', url)
+print ('\nurl: \n', url)
 html= urllib.request.urlopen (url).read()
-print ('\nhtml: ', html)
-x=input ('\nDevam')
+print ('\nhtml: \n', html)
+x=input ('\nDevam -1 soup : \n')
 
 soup= BeautifulSoup(html, 'html.parser')
-print('\nsoup :', soup)
-x=input('Devam?')
+print('\nsoup :\n', soup)
+x=input('\nDevam? - 2  tags  :\n')
 
 # Retrieve all of the anchor tags
 tags = soup ('a')
-print('tags : ', tags)
-z=input('Devam?')
+print('\ntags : \n', tags)
+z=input('\nDevam? - 3 href :   \n')
 
 for tag in tags:
     print (tag.get ('href', None))
